5816a6a30263793474f0c2e9e2d82cd3ed062edd
flevic <filip.levic@email.cz>
2023-11-23 22:41:49 +0100

    hdrp

---
+++ My project (4)/Assets/GameObjectChecker.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameObjectChecker : MonoBehaviour
+{
+    public GameObject[] gameObjects;
+
+    void Update()
+    {
+        // Check if EVERY game object from the list doesn't exist
+        if (AllGameObjectsNotExist())
+        {
+            // Do something when none of the game objects exist
+            SceneManager.LoadScene("LevelPicker");
+        }
+    }
+
+    bool AllGameObjectsNotExist()
+    {
+        // Iterate through the list of game objects
+        foreach (GameObject obj in gameObjects)
+        {
+            // If any game object exists, return false
+            if (obj != null)
+            {
+                return false;
+            }
+        }
+
+        // If none of the game objects exist, return true
+        return true;
+    }
+}
--- My project (4)/Assets/PROMETEO - Car Controller/Scripts/PrometeoCarController.cs
+++ My project (4)/Assets/PROMETEO - Car Controller/Scripts/PrometeoCarController.cs
@@ -410,7 +410,7 @@ public class PrometeoCarController : NetworkBehaviour
       if(useSounds){
         try{
           if(carEngineSound != null){
-            float engineSoundPitch = initialCarEngineSoundPitch + (Mathf.Abs(carRigidbody.velocity.magnitude) / 25f);
+            float engineSoundPitch = initialCarEngineSoundPitch + (Mathf.Abs(carRigidbody.velocity.magnitude) / 50f);
             carEngineSound.pitch = engineSoundPitch;
           }
           if((isDrifting) || (isTractionLocked && Mathf.Abs(carSpeed) > 12f)){
