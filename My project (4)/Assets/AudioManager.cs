92c59ca91e7b827688df2963aeb3da2bf68479c8
flevic <filip.levic@email.cz>
2023-12-03 20:23:24 +0100

    AudioUpdate

---
+++ My project (4)/Assets/AudioManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+    private AudioSource musicSource;
+
+    public AudioClip[] musicClips; // List of music clips
+
+    // Ensure that there is only one instance of AudioManager
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Don't destroy this object when loading a new scene
+
+            // Attach AudioSource to AudioManager
+            musicSource = gameObject.AddComponent<AudioSource>();
+            // Set other AudioSource properties
+            musicSource.loop = true;
+
+            // Randomly choose and play a music clip
+            if (musicClips.Length > 0)
+            {
+                AudioClip randomClip = musicClips[Random.Range(0, musicClips.Length)];
+                musicSource.clip = randomClip;
+                musicSource.Play();
+            }
+            else
+            {
+                Debug.LogError("No music clips assigned to AudioManager!");
+            }
+        }
+        else
+        {
+            Destroy(gameObject); // Destroy any additional AudioManager instances
+        }
+    }
+}
