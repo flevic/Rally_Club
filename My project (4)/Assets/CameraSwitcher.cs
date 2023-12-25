75db2b1099582c463d66a2e50c08a2084de11658
flevic <filip.levic@email.cz>
2023-12-25 19:38:17 +0100

    Things

--- My project (4)/Assets/CameraSwitcher.cs
+++ My project (4)/Assets/CameraSwitcher.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
-using UnityEditor.Rendering.LookDev;
+//using UnityEditor.Rendering.LookDev;
 using System;
 
 [Serializable]
