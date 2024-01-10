036b3c5b4b1b12d5a9f687244777274d2561bc9b
flevic <filip.levic@email.cz>
2024-01-10 11:50:50 +0100

    dataUpdate

--- My project (4)/Assets/SaveSystem/GameData.cs
+++ My project (4)/Assets/SaveSystem/GameData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class GameData
 {
 	//YOU CAN STORE ANYTHING: floats, integers, lists of bools lists of integers, even custom classes and stuff
-	
+
 	//Controls
 	public float MouseSensitivity = 1.5f;
 
@@ -16,37 +16,9 @@ public class GameData
 	public float MusicVol = 0f;
 	public float SpeechVol = 0f;
 
-	//Video
-	public int fov = 120;
-	
-	//IsWeaponUnlocked by index
-	public List<bool> weaponStatus = new List<bool>
+	public Dictionary<string, bool> levelsStatus = new Dictionary<string, bool>()
 	{
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false
-	};
-	
-	public List<int> equippedVariants = new List<int>
-	{
-		0,
-		0,
-		0,
-		0
+		{"Level1", false},
+		{"Level2", false}
 	};
 }
